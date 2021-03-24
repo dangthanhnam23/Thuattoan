a1e8f03332cefc4fa1b30743b77697077fad79a0
dangthanhnam23 <78284277+dangthanhnam23@users.noreply.github.com>
2021-03-25 03:56:34 +0700

    Update thuviencode.js

--- thuviencode.js
+++ thuviencode.js
@@ -1,1 +1,19 @@
 
+function getdecimal(num) {
+let x =  num;
+let a = "flase"; 
+let hundred = (x  > 100 ) ? x =  num.toPrecision(3) : "";
+let one = (x < 10 ) ? x =  num.toPrecision(1) : "";
+let ten = (x < 100 ) ? x =  num.toPrecision(2) : "";
+let thousand = (x > 1000) ?  x = num.toPrecision(4) : "";
+if ( x > num &&  x - 1 < num ) {
+   a = true;
+}else {
+  if ( x < num &&  x + 1 > num) {
+    a = "true"
+  }else{
+    a = "flase";
+  }
+}
+  return a
+}
